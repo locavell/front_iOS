d7dc5fa2c027478b9579d9601241f77890e209c0
heemings <xxyachuallyx@navet.com>
2024-07-15 15:13:31 +0900

    [baki] 파일 생성

---
+++ locavel-iOS/locavel-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  locavel-iOS
+//
+//  Created by 박희민 on 7/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ locavel-iOS/locavel-iOS/locavel_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  locavel_iOSApp.swift
+//  locavel-iOS
+//
+//  Created by 박희민 on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct locavel_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
