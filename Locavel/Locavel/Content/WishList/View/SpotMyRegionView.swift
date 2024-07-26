d86d4132a66ef7d9d5bb9a2fdc9db867eaa7b815
heemings <xxyachuallyx@navet.com>
2024-07-26 21:05:42 +0900

    [baki] SpotView 전체보기 기능

---
+++ Locavel/Locavel/Content/WishList/View/SpotMyRegionView.swift
@@ -0,0 +1,18 @@
+//
+//  SpotMyRegionView.swift
+//  Locavel
+//
+//  Created by 박희민 on 7/26/24.
+//
+
+import SwiftUI
+
+struct SpotMyRegionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotMyRegionView()
+}
