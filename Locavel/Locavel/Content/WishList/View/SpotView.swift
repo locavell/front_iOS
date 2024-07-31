e12b4dc3828c12fa9fc6f24178a47796432fabbe
heemings <xxyachuallyx@navet.com>
2024-07-31 23:50:27 +0900

    [baki] 수정

--- Locavel/Locavel/Content/WishList/View/SpotView.swift
+++ Locavel/Locavel/Content/WishList/View/SpotView.swift
@@ -4,9 +4,7 @@
 //
 //  Created by 박희민 on 7/21/24.
 //
-
 import SwiftUI
-
 
 struct SpotView: View {
     let localRestaurants = [
@@ -14,37 +12,34 @@ struct SpotView: View {
         Restaurant(image: "image2", name: "건강밥상마니 송도점", hours: "영업 시간: 11:00 - 22:00", rating: 4.2),
         Restaurant(image: "image3", name: "송도 슈블라", hours: "영업 시간: 11:00 - 21:00", rating: 4.4)
     ]
-    
+
     let favoriteRestaurants = [
         Restaurant(image: "image4", name: "어나더레벨", hours: "서울특별시 중구 을지로12길 11 4층", rating: 4.0),
         Restaurant(image: "image5", name: "꽃지로", hours: "서울특별시 중구 을지로12길 16 2층", rating: 4.6),
         Restaurant(image: "image6", name: "행2PM8P", hours: "서울특별시 중구 을지로12길 15 3층", rating: 4.2)
     ]
-    
+
     var body: some View {
-        ScrollView(){
+        ScrollView {
             VStack {
-                Spacer()
                 HStack {
                     Text("내 지역")
                         .font(.title2)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.bold)
                         .padding(.leading, 15)
                         .frame(maxWidth: .infinity, alignment: .leading)
-                    
-            
-                    // 전체보기 Button
-                    NavigationLink(destination: SpotMyRegionView()) {
+
+                    NavigationLink(destination: SpotMyRegionView(restaurants: localRestaurants)) {
                         Text("전체보기")
                             .font(.footnote)
                             .underline()
                             .foregroundColor(.gray)
-                            .frame(maxWidth: .infinity,alignment: .trailing)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
                             .padding()
                             .padding(.leading)
-                                        }
+                    }
                 }
-                
+
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack {
                         ForEach(localRestaurants) { restaurant in
@@ -54,22 +49,22 @@ struct SpotView: View {
                                     .aspectRatio(contentMode: .fill)
                                     .frame(width: 150, height: 150)
                                     .clipped()
-                                
+
                                 Text(restaurant.name)
                                     .font(.headline)
                                     .multilineTextAlignment(.center)
                                     .padding(.top, 5)
-                                
+
                                 Text(restaurant.hours)
                                     .font(.subheadline)
                                     .foregroundColor(.gray)
-                                
+
                                 HStack(spacing: 2) {
                                     ForEach(0..<5) { index in
                                         Image(systemName: index < Int(restaurant.rating) ? "star.fill" : "star")
                                             .foregroundColor(index < Int(restaurant.rating) ? .yellow : .gray)
                                     }
-                                    
+
                                     Text(String(format: "%.1f", restaurant.rating))
                                         .font(.subheadline)
                                         .foregroundColor(.gray)
@@ -80,29 +75,27 @@ struct SpotView: View {
                         }
                     }
                 }
-                
+
                 Spacer()
-                
-                
-                
+
                 HStack {
                     Text("관심 지역")
                         .font(.title2)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.bold)
                         .padding(.leading, 15)
                         .frame(maxWidth: .infinity, alignment: .leading)
-                    
-            
+
                     NavigationLink(destination: SpotInterestingView()) {
                         Text("전체보기")
                             .font(.footnote)
                             .underline()
                             .foregroundColor(.gray)
-                            .frame(maxWidth: .infinity,alignment: .trailing)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
                             .padding()
                             .padding(.leading)
-                                        }
+                    }
                 }
+
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack {
                         ForEach(favoriteRestaurants) { restaurant in
@@ -112,22 +105,22 @@ struct SpotView: View {
                                     .aspectRatio(contentMode: .fill)
                                     .frame(width: 150, height: 150)
                                     .clipped()
-                                
+
                                 Text(restaurant.name)
                                     .font(.headline)
                                     .multilineTextAlignment(.center)
                                     .padding(.top, 5)
-                                
+
                                 Text(restaurant.hours)
                                     .font(.subheadline)
                                     .foregroundColor(.gray)
-                                
+
                                 HStack(spacing: 2) {
                                     ForEach(0..<5) { index in
                                         Image(systemName: index < Int(restaurant.rating) ? "star.fill" : "star")
                                             .foregroundColor(index < Int(restaurant.rating) ? .yellow : .gray)
                                     }
-                                    
+
                                     Text(String(format: "%.1f", restaurant.rating))
                                         .font(.subheadline)
                                         .foregroundColor(.gray)
@@ -143,10 +136,8 @@ struct SpotView: View {
     }
 }
 
-
-
-
-#Preview {
-    SpotView()
+struct SpotView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpotView()
+    }
 }
-
