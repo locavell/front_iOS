810457b8099c5310c6dbd479472ba5c98a536e2f
7b7hom <cherishu1012@gmail.com>
2024-08-04 14:53:46 +0900

    [cookie] 빌드 오류 해결, 네아로 기본 설정

--- Locavel/Pods/Target Support Files/Pods-Locavel-LocavelUITests/Pods-Locavel-LocavelUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Locavel_LocavelUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Locavel_LocavelUITests
-@end
--- Locavel/Pods/Target Support Files/Pods-LocavelTests/Pods-LocavelTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LocavelTests : NSObject
-@end
-@implementation PodsDummy_Pods_LocavelTests
-@end
