426d5d7ec4b10d47e51f6c147e4fda50e51734d8
heemings <xxyachuallyx@navet.com>
2024-08-20 17:40:51 +0900

    [baki] Activity 뷰 생성

---
+++ Locavel/Locavel/Content/WishList/View/Activity/ActivityInterestingView.swift
@@ -0,0 +1,15 @@
+//
+//  ActivityInterestingView.swift
+//  Locavel
+//
+//  Created by 박희민 on 8/20/24.
+//
+
+import SwiftUI
+
+struct ActivityInterestingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
--- Locavel/Locavel/Content/WishList/View/Food/FoodInterestingView.swift
+++ Locavel/Locavel/Content/WishList/View/Food/FoodInterestingView.swift
@@ -1,18 +1,18 @@
 //
-//  ActivityView.swift
+//  FoodInterestingView.swift
 //  Locavel
 //
-//  Created by 박희민 on 7/21/24.
+//  Created by 박희민 on 8/19/24.
 //
 
 import SwiftUI
 
-struct ActivityView: View {
+struct FoodInterestingView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ActivityView()
+    FoodInterestingView()
 }
