2a58cad139e96fdd303979961d29308d57a92190
7b7hom <cherishu1012@gmail.com>
2024-08-22 19:44:56 +0900

    [cookie] SignUpView 작업 중

---
+++ Locavel/Locavel/Content/SignUp/Components/CustomTextFieldStyle.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTextField.swift
+//  Locavel
+//
+//  Created by 김경서 on 8/22/24.
+//
+
+import SwiftUI
+
+struct CustomTextField: TextFieldStyle {
+    
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color.green)
+                .cornerRadius(8)
+                .frame(height: 46)
+            
+            // 텍스트필드
+            configuration
+                .font(.title)
+                .padding()
+        }
+    }
+}
