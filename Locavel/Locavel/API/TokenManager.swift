7176c33386eb8a0f89acc67188459dd636f18c01
kuj7373 <kuj7373@naver.com>
2024-08-24 05:31:30 +0900

    PlaceDetail 충돌

--- Locavel/Locavel/API/TokenManager.swift
+++ Locavel/Locavel/API/TokenManager.swift
@@ -12,10 +12,6 @@ class TokenManager {
     
     var accessToken: String {
         // Return the access token here
-<<<<<<< HEAD
         return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcyNDQzMzY4MSwiZW1haWwiOiJlZDFAZXhhbXBsZS5jb20ifQ.u3gNJ6Z6Wr0s7o9guTbs6ogmsOheIqc79WumDOjeSNtgQwSAs3ZRVBVbTMgXpgBoC0XqX6S-Y3rtpZR0Stw2mw"
-=======
-        return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcyNDQzNjYxOSwiZW1haWwiOiJoZWVtaW5nQGV4YW1wbGUuY29tIn0.X3thwXvZ6MXwzN3j0zrN2htYWvDWgSKI3aU9oGvDoOGlxkAkTi_wGGtXvJXlR3vWdT-R5szP2OIdjp30tP6FIw"
->>>>>>> 50ae358ee7b34751eab9b9fec273012f1a041acd
     }
 }
