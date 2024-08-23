df41bc33f71a617522877da744ae2dae3166a938
kuj7373 <kuj7373@naver.com>
2024-08-24 01:26:01 +0900

    .

--- Locavel/Locavel/API/TokenManager.swift
+++ Locavel/Locavel/API/TokenManager.swift
@@ -12,6 +12,6 @@ class TokenManager {
     
     var accessToken: String {
         // Return the access token here
-        return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcyNDQxMTI3NSwiZW1haWwiOiJlZDFAZXhhbXBsZS5jb20ifQ.4HttlRHaDLX5mUrtU0R3siTqSZViEwtDiV0T0xKXDRfu9htqbyslbgj8gKPecoJLfV42dO3o_8m3XSFOc8macQ"
+        return "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcyNDQzMzY4MSwiZW1haWwiOiJlZDFAZXhhbXBsZS5jb20ifQ.u3gNJ6Z6Wr0s7o9guTbs6ogmsOheIqc79WumDOjeSNtgQwSAs3ZRVBVbTMgXpgBoC0XqX6S-Y3rtpZR0Stw2mw"
     }
 }
